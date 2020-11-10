5199a871bfedc7b163bd4f82a34be8d59fec2932
carrye <carryegaulin033178@gmail.com>
2020-11-10 10:18:59 -0500

    post hurricane work

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
 const express = require('express')
+const showdata = require('./showData')
 const showData = require('./showData')
 const app = express()
 
 app.set('view engine', 'pug')
-
+app.use(express.static('public'))
 app.get('/', (request, response) => {
   return response.render('index', { showData })
 })
 
-app.get('/showData/:title', (request, response) => { // colon important it says HEY ITS A VARIABLE
-  const show = showData.find((show) => show.title === parseInt(request.params.title)) // THIS IS HOW YOU GET PARAM VALUES FROM THE URL!!!!!!
+app.get('/seasons/:id', (request, response) => {
+  const show = showdata.seasons[request.params.id - 1]
 
-  return response.render('showData', { show })
+  return response.render('seasons', { show })
 })
-
-app.get('/showData', function (req, res, next) {
-  const directors = ['Ins Choi', 'Kevin White']
-
-  res.render('index', {
-    directors: directors
-  })
-})
-
-app.get('/showData', function (req, res, next) {
-  var starring = ['Paul Sun-Hyung Lee', 'Jean Yoon', 'Andrea Bang', 'Simu Liu', 'Andrew Phung', 'Nicole Power']
-
-  res.render('index', {
-    starring: starring
-  })
-})
-
-app.use(express.static('public'))
-
 
 app.all('*', (request, response) => {
   return response.sendStatus(404)
